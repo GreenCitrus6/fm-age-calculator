ca34c23d80617ba7eb1e83b4c00cf4d0ad70c2d7
Daniel A <DanielAAad01@gmail.com>
2023-12-18 02:53:58 -0800

    Added custom fonts, colors to tailwind.config.js. Set root em to 32px.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
       },
+      colors: {
+        'primary': {
+          'purple': 'hsl(259, 100%, 65%)',
+          'red': 'hsl(0, 100%, 67%)',
+        },
+        'neutral': {
+          'offwhite': 'hsl(0, 0%, 94%)',
+          'lightgrey': 'hsl(0, 0%, 86%)',
+          'smokeygrey': 'hsl(0, 1%, 44%)',
+          'offblack': 'hsl(0, 0%, 8%)',
+        }
+      }
     },
   },
   plugins: [],
